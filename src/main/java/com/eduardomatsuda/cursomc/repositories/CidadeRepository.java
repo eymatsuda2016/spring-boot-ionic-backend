ba3b414b7d33e5dfc0a66ccd74ea83f86c900ae9
eymatsuda2016 <eymatsuda2016@outlook.com>
2022-09-01 21:38:08 -0300

    Estado e Cidade

---
+++ src/main/java/com/eduardomatsuda/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.eduardomatsuda.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardomatsuda.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
