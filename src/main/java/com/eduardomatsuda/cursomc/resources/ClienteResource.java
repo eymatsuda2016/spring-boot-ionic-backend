86a6666fbda7c6cf6411d267bcdee618794322b2
eymatsuda2016 <eymatsuda2016@outlook.com>
2022-09-02 22:45:15 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/eduardomatsuda/cursomc/resources/ClienteResource.java
+++ src/main/java/com/eduardomatsuda/cursomc/resources/ClienteResource.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eduardomatsuda.cursomc.domain.Categoria;
-import com.eduardomatsuda.cursomc.services.CategoriaService;
+import com.eduardomatsuda.cursomc.domain.Cliente;
+import com.eduardomatsuda.cursomc.services.ClienteService;
 
 
 
 @RestController
-@RequestMapping(value = "/categorias")
-public class CategoriaResource {
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value= "/{id}", method = RequestMethod.GET)
 	public ResponseEntity <?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.find(id);
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok(). body(obj);
 	}
 }
