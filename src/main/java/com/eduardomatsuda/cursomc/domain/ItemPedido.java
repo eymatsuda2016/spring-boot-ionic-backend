3998245d54afbf759a93130a933beb8491fb6c6f
eymatsuda2016 <eymatsuda2016@outlook.com>
2022-09-12 22:32:00 -0300

    Endpoint /Pedidos{id} disponibilizado

--- src/main/java/com/eduardomatsuda/cursomc/domain/ItemPedido.java
+++ src/main/java/com/eduardomatsuda/cursomc/domain/ItemPedido.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -29,19 +32,19 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
 	}
 	
 	public ItemPedidoPK getId() {
 		return id;
 	}
 	
-	public Produto getProduto() {
-		return id.getProduto();
-	}
-
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
